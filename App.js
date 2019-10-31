cd951434297422857920ba4c675b5fa236e65ed7
Anton Filiptsov <anton@tryelevate.com>
2019-10-31 15:44:14 -0400

    added a flatlist

--- App.js
+++ App.js
@@ -4,11 +4,12 @@ import { StyleSheet, Text, View, Button } from 'react-native';
 export default function App() {
   const [outputText, setOutputText] = useState('Open up App.js to start working on your app!')
   return (
-    <View style={styles.container}>
+    <View style={styles.container}gi>
       <Text>{outputText}</Text>
       <Button title="Change Text" onPress ={()=> setOutputText('The text changed!')} />
       <Button>Next Page Link</Button>
       <TextInput placeholder="Case Number" />
+      <FlatList></FlatList>
     </View>
   );
 }
